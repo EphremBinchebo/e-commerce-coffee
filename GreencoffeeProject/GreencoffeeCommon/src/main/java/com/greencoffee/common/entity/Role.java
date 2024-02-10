6713f4d264008be2aa2ba168efefaed14961e648
EPHREM bINCHEBO <ephremgensa1@gmail.com>
2024-02-10 18:49:44 -0500

    init

--- GreencoffeeProject/GreencoffeeCommon/src/main/java/com/greencoffee/common/entity/Role.java
+++ GreencoffeeProject/GreencoffeeCommon/src/main/java/com/greencoffee/common/entity/Role.java
@@ -20,5 +20,46 @@ public class Role {
 	
 	@Column(length = 150, nullable = false, unique = true)
 	private String description;
+    
+	
+	public Role() {
+		
+	}
+    
+	
+	public Role(String name) {
+		this.name = name;
+	}
 
+
+	public Role(String name, String description) {
+		this.name = name;
+		this.description = description;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+    
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	
 }
--- GreencoffeeProject/GreencoffeeWebParent/GreencoffeeBackEnd/src/main/java/com/greencoffee/admin/GreencoffeeBackEndApplication.java
+++ GreencoffeeProject/GreencoffeeWebParent/GreencoffeeBackEnd/src/main/java/com/greencoffee/admin/GreencoffeeBackEndApplication.java
@@ -2,9 +2,11 @@ package com.greencoffee.admin;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
 @SpringBootApplication
+@EntityScan({"com.greencoffee.common.entity", "com.greencoffee.admin.user"})
 public class GreencoffeeBackEndApplication {
 
 	public static void main(String[] args) {
