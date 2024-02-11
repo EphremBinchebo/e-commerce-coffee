f5ded5754dbf53481f628ee177ce569d5cd0f8da
EPHREM bINCHEBO <ephremgensa1@gmail.com>
2024-02-10 19:18:53 -0500

    init

---
+++ GreencoffeeProject/GreencoffeeCommon/src/main/java/com/greencoffee/common/entity/User.java
@@ -0,0 +1,5 @@
+package com.greencoffee.common.entity;
+
+public class User {
+
+}
--- GreencoffeeProject/GreencoffeeWebParent/GreencoffeeBackEnd/src/test/java/com/greencoffee/admin/user/RoleRepositoryTest.java
+++ GreencoffeeProject/GreencoffeeWebParent/GreencoffeeBackEnd/src/test/java/com/greencoffee/admin/user/RoleRepositoryTest.java
@@ -2,11 +2,14 @@ package com.greencoffee.admin.user;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import java.util.List;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase.Replace;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.annotation.Rollback;
 
 //import com.greencoffee.admin.user.RoleRepository;
 
@@ -14,6 +17,7 @@ import com.greencoffee.common.entity.Role;
 
 @DataJpaTest
 @AutoConfigureTestDatabase(replace = Replace.NONE)
+@Rollback(false)
 public class RoleRepositoryTest {
 
 	 @Autowired
@@ -25,5 +29,19 @@ public class RoleRepositoryTest {
 		 Role saveRole = repo.save(roleAdmin);
 		 
 		 assertThat(saveRole.getId()).isGreaterThan(0);
+	 }
+	 
+	 @Test
+	 public void testCreateRestRoles() {
+		 Role roleSalesperson = new Role("Salesperson", "manage product price, "
+	     + "customers, shipping, orders and sales report");
+		 
+		 Role roleEditor = new Role("Editor", "manage categories, brands, "
+				 + "products, articles and menus");
+		 Role roleShipper = new Role("shipper", "view products, view orders "
+				 + "and update order status");
+		 Role roleAssistant = new Role("Assistant", "manage questions and reviews");
+	     
+		 repo.saveAll(List.of(roleSalesperson, roleEditor, roleShipper, roleAssistant));
 	 }
 }
